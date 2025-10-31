5066397f1179c423923f98a999ddc14fd8630e59
TAKSHDIYORA <takshdiyoradiyora@gmail.com>
2025-10-31 11:31:26 +0530

    third

--- FOREVER-main/back-end/server.js
+++ FOREVER-main/back-end/server.js
@@ -15,8 +15,11 @@ const PORT = process.env.PORT || 4000;
 connectCloudinary();
 //middlewares
 app.use(express.json());
-app.use(cors())
-
+app.use(cors({
+  origin: "https://forever-front-end-zeta.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 // api endpoints 
 app.use('/api/user',userRouter);
 app.use('/api/product',productRouter);
